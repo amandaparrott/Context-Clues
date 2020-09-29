da770569bcfdcc64a59bdef6ba9324fccf36181d
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-09-29 17:08:24 -0500

    fixed alert message

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 $(document).ready(function () {
 
     //arrays
-
-    // var friends;
-    // var locations;
-    // var weapons;
+    var friends = ['Frodo', 'The 13th Doctor', 'Pickle Rick', 'Louise Belcher', 'Paul Rudd'];
+    var locations = ['an alternate universe', 'the litter box', 'a for loop', 'the tardis', 'a time vortex', 'the shire', 'Central Perk', 'a gas station bathroom', 'the Wonder Wharf', 'a tiny clown car'];
+    var weapons = ['used bandaid', 'banana', 'cherry coke', 'fragile masculinity', 'burger of the day', 'original 1970s Batman figurine', 'thrift store sunlgasses', 'toenail clippings', 'apple martini', 'impending sense of doom', 'sugar plums', 'vegan cheese', 'highlights magazine', 'tic-tac-toe code', 'quarantine', 'label maker', 'kittens', 'snakes on a plane', 'for loop', 'broken lightbulb'];
 
     for (var i = 1; i < 101; i++) {
         //create h3's
         var $h3 = $('<h3>Accusation Number ' + i + '</h3>');
         $($h3).appendTo('div');
-        $($h3).click(function () {
-            //arrays
-            friends = ['Frodo', 'The 13th Doctor', 'Pickle Rick', 'Louise Belcher', 'Paul Rudd'];
-            locations = ['an alternate universe', 'the litter box', 'a for loop', 'the tardis', 'a time vortex', 'the shire', 'Central Perk', 'a gas station bathroom', 'the Wonder Wharf', 'a tiny clown car'];
-            weapons = ['used bandaid', 'banana', 'cherry coke', 'fragile masculinity', 'burger of the day', 'original 1970s Batman figurine', 'thrift store sunlgasses', 'toenail clippings', 'apple martini', 'impending sense of doom', 'sugar plums', 'vegan cheese', 'highlights magazine', 'tic-tac-toe code', 'quarantine', 'label maker', 'kittens', 'snakes on a plane', 'for loop', 'broken lightbulb'];
-//randomize arrays
-            var friends = friends[Math.floor(Math.random() * friends.length)];
-            var locations = locations[Math.floor(Math.random() * locations.length)];
-            var weapons = weapons[Math.floor(Math.random() * weapons.length)];
-            //alert
-            alert('Accusation' + i + ": I accuse " + friends + " with the " + weapons + " in " + locations + "!");
-        });
+        $($h3).click(makeAlert(i));
     };
-
-
-
-
+    function makeAlert(i) {
+        //randomize arrays
+        var friendsBlah = friends[Math.floor(Math.random() * friends.length)];
+        var locationsBlah = locations[Math.floor(Math.random() * locations.length)];
+        var weaponsBlah = weapons[Math.floor(Math.random() * weapons.length)];
+        function accusationAlert() {
+            alert('Accusation' + i + ": I accuse " + friendsBlah + " with the " + weaponsBlah + " in " + locationsBlah + "!");
+        };
+        return accusationAlert;
+    };
 
 
     //end of body
