7beb6fdefdf248896a5328ba5cd92b36fde570ed
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-09-29 12:43:21 -0500

    added loop for h3s

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+
+for (var i = 1; i < 101; i++) {
+     $('.accusations').append('<h3 id="number ' + i + '">Accusation Number ' + i + '</h3>');
+}
+
+    //end of body
+});
