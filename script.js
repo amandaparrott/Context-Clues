b41c9fc21330512d7dcd8e7945e20609faeb0b84
Amanda Parrott <amandaparrott@amandas-air.attlocal.net>
2020-09-30 13:33:48 -0500

    cleaned up notes

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
     var weapons = ['used bandaid', 'banana', 'cherry coke', 'fragile masculinity', 'burger of the day', 'original 1970s Batman figurine', 'thrift store sunlgasses', 'toenail clippings', 'apple martini', 'impending sense of doom', 'sugar plums', 'vegan cheese', 'highlights magazine', 'tic-tac-toe code', 'quarantine', 'label maker', 'kittens', 'snakes on a plane', 'for loop', 'broken lightbulb'];
 
     for (var i = 1; i < 101; i++) {
-        //create h3's
+        //create 100 h3's
         var $h3 = $('<h3>Accusation Number ' + i + '</h3>');
         $($h3).appendTo('div');
+        //listen for click on h3's
         $($h3).click(makeAlert(i));
     };
+    //create accusations
     function makeAlert(i) {
         //randomize arrays
         var friendsBlah = friends[Math.floor(Math.random() * friends.length)];
@@ -19,6 +21,7 @@ $(document).ready(function () {
         function accusationAlert() {
             alert('Accusation' + i + ": I accuse " + friendsBlah + " with the " + weaponsBlah + " in " + locationsBlah + "!");
         };
+        //alert accusation on click
         return accusationAlert;
     };
 
